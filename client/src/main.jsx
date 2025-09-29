ae73562faab1b1c1fe11988ca9e552ead82c93e0
elisha <elishajameel2007@gmail.com>
2025-09-29 17:34:54 +0500

    added circuit from io

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </StrictMode>
+);
--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const port = 3000;
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  console.log("id", socket.id);
 });
 
 app.listen(port, () => {
