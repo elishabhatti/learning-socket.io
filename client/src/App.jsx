12314ae2ae482fbebc61d80e24f611ec9c824dfe
elisha <elishajameel2007@gmail.com>
2025-09-29 17:39:28 +0500

    used cors in app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { io } from "socket.io-client"
 
 const App = () => {
+  const socket = io("http://localhost:3000")
   return (
     <div>App</div>
   )
--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {});
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
